604c3fddcce2f7f452b68cd5ae0f4e624ff82c1d
unknown <magosdk@icloud.com>
2022-12-06 16:23:40 +0100

    +roles, admin and user, token auth, access - all done

--- authController.js
+++ authController.js
@@ -1,10 +1,26 @@
 const User = require("./models/User")
 const Role = require("./models/Role")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const { validationResult } = require("express-validator")
+const { secret } = require("./config")
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  }
+  return jwt.sign(payload, secret, { expiresIn: "24h" })
+}
 
 class authController {
   async signup(req, res) {
     try {
+      // express-validation
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: "Error in signup", errors })
+      }
       // destruct
       const { username, password } = req.body
       // find user in db, if not continue
@@ -31,8 +47,19 @@ class authController {
       res.status(400).json({ message: "Registration error" })
     }
   }
-  async signin(req, res) {
+  async login(req, res) {
     try {
+      const { username, password } = req.body
+      const user = await User.findOne({ username })
+      if (!user) {
+        return res.status(400).json({ message: `User ${username} not found!` })
+      }
+      const validPassword = bcrypt.compareSync(password, user.password)
+      if (!validPassword) {
+        return res.status(400).json({ message: `Wrong password!` })
+      }
+      const token = generateAccessToken(user._id, user.roles)
+      return res.json({ token })
     } catch (e) {
       console.log(e)
       res.status(400).json({ message: "Login error" })
@@ -45,6 +72,9 @@ class authController {
       // const adminRole = new Role({ value: "Admin" })
       // await userRole.save()
       // await adminRole.save()
+
+      const users = await User.find()
+      res.json(users)
       res.json("getusers: server work")
     } catch (e) {}
   }
--- authRouter.js
+++ authRouter.js
@@ -1,11 +1,24 @@
 const Router = require("express")
 const router = new Router()
 const controller = require("./authController")
+const { check } = require("express-validator")
+const authMiddleware = require("./middlewares/authMiddleware")
+const roleMiddleware = require("./middlewares/roleMiddleware")
 
 // Routes
 
-router.post("/signup", controller.signup)
-router.post("/signin", controller.signin)
-router.get("/users", controller.getUsers)
+router.post(
+  "/signup",
+  [
+    check("username", "Username can not be empty").notEmpty(),
+    check("password", "Password must be between 4 and 10 symboles").isLength({
+      min: 4,
+      max: 10,
+    }),
+  ],
+  controller.signup
+)
+router.post("/login", controller.login)
+router.get("/users", roleMiddleware(["Admin"]), controller.getUsers)
 
 module.exports = router
